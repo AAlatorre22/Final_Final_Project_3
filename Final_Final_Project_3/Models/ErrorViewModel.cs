3e334a997f6f70b30ff977180125aae7b6d2db44
AAlatorre22 <126432462+AAlatorre22@users.noreply.github.com>
2023-04-25 13:33:29 -0700

    Add project files.

---
+++ Final_Final_Project_3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Final_Final_Project_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
